27711845b48b06ee420ca65ca8448502ae429ea2
twozeronine <amdusias209@gmail.com>
2024-03-25 23:45:20 +0900

    🔥 Remove comment & 📝 Add Docs

--- lib/elixir_vending_machine.ex
+++ lib/elixir_vending_machine.ex
@@ -1,17 +1,4 @@
 defmodule ElixirVendingMachine do
-  @moduledoc """
-  Documentation for `ElixirVendingMachine`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ElixirVendingMachine.hello()
-      :world
-
-  """
   def hello do
     :world
   end
