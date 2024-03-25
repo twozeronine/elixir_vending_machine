2414c9afe5c1031538d9eef5bdba460dbefaceef
twozeronine <amdusias209@gmail.com>
2024-03-25 23:52:37 +0900

    ✨ Implement Switch module

---
+++ lib/switch.ex
@@ -0,0 +1,17 @@
+defmodule Switch do
+  use GenStateMachine
+
+  # Callbacks
+
+  def handle_event(:cast, :flip, :off, data) do
+    {:next_state, :on, data + 1}
+  end
+
+  def handle_event(:cast, :flip, :on, data) do
+    {:next_state, :off, data}
+  end
+
+  def handle_event({:call, from}, :get_count, state, data) do
+    {:next_state, state, data, [{:reply, from, data}]}
+  end
+end
