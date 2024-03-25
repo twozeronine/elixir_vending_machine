e1b5bbe2753a228cdb12e1d9a68ed93d5b17f85f
Leeyoungkyu <amdusias209@gmail.com>
2024-03-26 01:18:40 +0900

    ✨ Implement Switch using state_enter (#3)

--- lib/switch.ex
+++ lib/switch.ex
@@ -1,19 +1,19 @@
 defmodule Switch do
-  use GenStateMachine, callback_mode: :state_functions
+  use GenStateMachine, callback_mode: [:handle_event_function, :state_enter]
 
-  def off(:cast, :flip, data) do
-    {:next_state, :on, data + 1}
+  def handle_event(:enter, _event, state, data) do
+    {:next_state, state, %{data | enters: data.enters + 1}}
   end
 
-  def off({:call, from}, :get_count, data) do
-    {:keep_state_and_data, [{:reply, from, data}]}
+  def handle_event(:cast, :flip, :off, data) do
+    {:next_state, :on, %{data | flips: data.flips + 1}}
   end
 
-  def on(:cast, :flip, data) do
+  def handle_event(:cast, :flip, :on, data) do
     {:next_state, :off, data}
   end
 
-  def on({:call, from}, :get_count, data) do
+  def handle_event({:call, from}, :get_count, _state, data) do
     {:keep_state_and_data, [{:reply, from, data}]}
   end
 end
